9b521f11a2cf227ce5d4120166918f8d95891277
kelyjahaburbe95-wq <kelyjahaburbe95@gmail.com>
2025-12-11 10:25:18 +0100

    Update home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import ProductCard from "../components/ProductCard";
+import ProductCard from "../components/productcards.jsx";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
