3ccb23f8bad20747d792e0065622642c8299f45d
kelyjahaburbe95-wq <kelyjahaburbe95@gmail.com>
2025-12-11 02:40:08 +0100

    Update produits.js

--- backend/src/produits.js
+++ backend/src/produits.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 
-const router = express.Router();
 const prisma = new PrismaClient();
+const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const products = await prisma.product.findMany();
-  res.json(products);
+  const produits = await prisma.produit.findMany();
+  res.json(produits);
 });
 
 export default router;
