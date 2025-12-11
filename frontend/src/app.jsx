156090ec227aa48fd586fc8a9c8e2ddf474997e5
kelyjahaburbe95-wq <kelyjahaburbe95@gmail.com>
2025-12-11 03:01:40 +0100

    Create main App component with routing

---
+++ frontend/src/app.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
