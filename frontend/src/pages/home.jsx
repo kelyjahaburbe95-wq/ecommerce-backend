6a8a13c3b32da5aafbcb4cd744303368c302d406
kelyjahaburbe95-wq <kelyjahaburbe95@gmail.com>
2025-12-11 14:00:04 +0100

    Update home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,19 +1,20 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import ProductCards from "../components/productcards.jsx";
+import ProductCard from "../components/productcards.jsx";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get(import.meta.env.VITE_API_URL + "/products")
-      .then(res => setProducts(res.data));
+    axios.get(`${import.meta.env.VITE_API_URL}/products`)
+      .then(res => setProducts(res.data))
+      .catch(err => console.log(err));
   }, []);
 
   return (
     <div className="p-4 grid grid-cols-2 gap-4">
       {products.map(p => (
-        <ProductCards key={p.id} product={p} />
+        <ProductCard key={p.id} product={p} />
       ))}
     </div>
   );
